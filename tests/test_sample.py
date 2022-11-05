adc3211bccce11fe33aaa03a60b66d4d2e14d771
“tmp7125” <not_exist@not_exist.com>
2022-11-04 21:44:56 -0400

    Made firefox running in headless mode

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -4,7 +4,11 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 
 def test_sample():
-    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    o = webdriver.FirefoxOptions()
+    o.headless = True
+    driver = webdriver.Firefox(
+        service=FirefoxService(GeckoDriverManager().install()), options=o
+    )
     driver.get('https://www.saucedemo.com/')
 
     assert driver.title == 'Swag Labs'
