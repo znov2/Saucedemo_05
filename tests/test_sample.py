21ec641ef7f17810c4cda9db4746f06b8508d265
znov2 <znov2017@gmail.com>
2022-11-05 20:19:16 -0700

    inition commit

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,14 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 
 def test_sample():
-    o = webdriver.FirefoxOptions()
+    o = webdriver.ChromeOptions()
     o.headless = True
-    driver = webdriver.Firefox(
-        service=FirefoxService(GeckoDriverManager().install()), options=o
-    )
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get('https://www.saucedemo.com/')
 
     assert driver.title == 'Swag Labs'
